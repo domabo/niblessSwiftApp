3207202c93996486cb09f4aca44b6d5c57f6b1ab
Guy Barnard <guy@Guys-MacBook-Air.local>
2014-08-27 16:24:53 -0500

    Added unversioned files

---
+++ niblessSwiftApp/app.swift
@@ -0,0 +1,27 @@
+//
+//  app.swift
+//
+//  Copyright (c) 2014 Domabo. All rights reserved.
+//
+
+
+import Cocoa
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+    var controller: NSWindowController = NiblessWindowController()
+    let app: NSApplication
+    
+    init(app: NSApplication) {
+        self.app = app
+    }
+    
+    func applicationDidFinishLaunching(aNotification: NSNotification?) {
+        println("application started!")
+        controller.showWindow(nil)
+        app.activateIgnoringOtherApps(true)
+    }
+    
+    func applicationWillTerminate(aNotification: NSNotification?) {
+        println("application terminated!")
+    }
+}
---
+++ niblessSwiftApp/controller.swift
@@ -0,0 +1,25 @@
+//
+//  controller.swift
+//
+//  Copyright (c) 2014 Domabo. All rights reserved.
+//
+
+import Cocoa
+
+class NiblessWindowController: NSWindowController {
+    init() {
+        super.init(window: nil)
+        let rect   = NSMakeRect(100, 100, 400, 400)
+        let window = NSWindow(contentRect:rect,
+            styleMask:self.windowStyleMask(),
+            backing:.Buffered,
+            defer:false)
+        window.title = "Nibless Swift Window"
+        self.window = window
+    }
+    
+    func windowStyleMask() -> Int {
+        return NSTitledWindowMask | NSClosableWindowMask | NSResizableWindowMask
+    }
+}
+
