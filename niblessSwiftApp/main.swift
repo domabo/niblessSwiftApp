042b2f298c94850459e6badb108dad4074491701
Guy Barnard <guy@Guys-MacBook-Air.local>
2014-08-27 16:14:25 -0500

    Initial Commit

---
+++ niblessSwiftApp/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  niblessSwiftApp
+//
+//  Created by Guy Barnard on 8/27/14.
+//  Copyright (c) 2014 Guy Barnard. All rights reserved.
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
